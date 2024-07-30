1f8dfa9cc406343f2d95ebc2e00677c98bceed2f
Khalil <k.almaasali@smsc.sa>
2024-07-30 22:49:58 +0300

    fix url port from 8088 and error msg #11

--- book-lending-ui/src/app/app.module.ts
+++ book-lending-ui/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login.component';
+import { LoginComponent } from './pages/login/login.component';
+
 @NgModule({
   declarations: [
     AppComponent,
--- book-lending-ui/src/app/pages/login/login.component.ts
+++ book-lending-ui/src/app/pages/login/login.component.ts
@@ -1,25 +1,47 @@
 import { Component } from '@angular/core';
-import { AuthenticationRequest } from '../services/models';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/services';
+import { AuthenticationRequest } from '../../services/models';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
 
-  authRequest: AuthenticationRequest = {
-    email: '',
-    password: ''
-  };
-  errorMsg:Array<string>=[];
+  authRequest: AuthenticationRequest = {email: '', password: ''};
+  errorMsg: Array<string> = [];
 
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService//,
+    //private tokenService: TokenService
+  ) {
+  }
 
   login() {
-    throw new Error('Method not implemented.');
-    }
-    register() {
-    throw new Error('Method not implemented.');
-    }
-  
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res) => {
+       // this.tokenService.token = res.token as string;
+        this.router.navigate(['books']);
+      },
+      error: (err) => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push(err.error.errorMsg);
+        }
+      }
+    });
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
 }
